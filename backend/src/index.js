f938d1b9ef8f4e3464a2f81b2b40062ae987a62a
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2024-04-25 15:49:54 -0300

    add mongoose connection to database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello world");
+import express from "express";
+import { mongoose } from "mongoose";
+
+const app = express();
+
+//middleware
+app.use(express.json());
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/CRUD");
+
+export default app;
