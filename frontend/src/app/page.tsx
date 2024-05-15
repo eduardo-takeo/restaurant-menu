d8cf30ce5a75a18af2501ae8a1a16ab1662e131d
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2024-05-14 22:38:16 -0300

    install tailwindcss

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main>Hello world</main>;
+  return (
+    <main>
+      <h1 className="text-lg font-bold object-center">Hello world</h1>
+    </main>
+  );
 }
