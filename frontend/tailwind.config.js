92ee52adbae5fdda0e1f18ea6e5cc965a94c831b
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2024-05-19 22:38:40 -0300

    style: add custom colors to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#F2B64E",
+      gray: {
+        100: "#F3F1F0",
+        700: "#717171",
+      },
+    },
   },
   plugins: [],
 };
