dcd67625dcef3ba49da10b56244d0c80bcf50097
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2024-05-23 22:55:52 -0300

    style: remove background color from Footer

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="bg-white text-sm text-gray-700 py-8 flex justify-center align-middle">
+    <footer className="text-sm text-gray-700 py-8 flex justify-center align-middle">
       <p>
         Made by <b className="text-gray-400">restaurant-menu</b>
       </p>
